de6b5d6d3d463bb7a2f39947242fe3b845bf5f1d
code1719 <codyhearell@gmail.com>
2023-12-07 10:14:42 -0700

    noticed problem with previous branch

--- Documents/Sites2/nodeJsTaskManager/node-express-course-main/03-task-manager/starter/routes/tasks.js
+++ Documents/Sites2/nodeJsTaskManager/node-express-course-main/03-task-manager/starter/routes/tasks.js
@@ -1,18 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const {getAllTasks, createTask, getTask, updateTask, deleteTask} = require ('../controllers/tasks');
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/tasks");
 // const {getAllTasks} = require('../')
 
-
-router.route('/').get(getAllTasks).post(createTask)
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
-
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
 // router.route('/').get(getAllTasks)
 // router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
-    // router.route('/').get((req, res) => {
-    //     res.send('all items')
-    // })
+// router.route('/').get((req, res) => {
+//     res.send('all items')
+// })
 
-module.exports = router 
+module.exports = router;
